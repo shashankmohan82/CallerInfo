90ddffe4a2aa06759d9faf41a76836fdf2344e27
Shashank <shashank.mohan82@gmail.com>
2016-10-06 23:26:23 +0530

    changed caller tag for dialog
    better look for caller dialog

--- app/src/main/java/shashank/com/callerinfo/OverlayActivity.java
+++ app/src/main/java/shashank/com/callerinfo/OverlayActivity.java
@@ -100,7 +100,7 @@ public class OverlayActivity extends AppCompatActivity {
 
                 if(cl.isSpam(number)){
 
-                   imageView.setImageResource(R.drawable.tag_red);
+                   imageView.setImageResource(R.drawable.tagl);
                 }
                 else {
                     imageView.setImageResource(R.drawable.tag_green);
